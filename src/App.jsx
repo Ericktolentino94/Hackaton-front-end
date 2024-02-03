34f0758e98115dbbea82686c2ac77f81e0b294b4
Alexander Tsiklidis <alex@Alexanders-MacBook-Air.local>
2024-02-03 16:29:42 -0500

    makes changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,27 @@ import { UserProvider } from "./Providers/UserProvider";
 import { LoggedInPage } from "./Pages/LoggedInPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import  { MemoGoogleMap } from "./Components/googleMap/GoogleMap.jsx"
+import { useState } from "react";
 
 
 function App() {
-  
+  const[currentUser, setCurrentUser] = useState(null);
   return (
     <div className="App">
      <header className="App-header"></header>
       <UserProvider>
         <Router>
           <Routes>
-            <Route  path="/" element = { <LoginPage /> } /> 
-            <Route path="/loggedInPage" element = { <LoggedInPage  />} />
+            <Route  path="/" element = { <LoginPage  /> } /> 
+            <Route path="/loggedInPage" element = { <LoggedInPage currentUser={currentUser} 
+            setCurrentUser={setCurrentUser}/>} />
          </Routes>
         </Router> 
 
        </UserProvider> 
       <main>
-        <MemoGoogleMap />
+        <MemoGoogleMap 
+        currentUser={currentUser} />
       </main>
 
     </div>
