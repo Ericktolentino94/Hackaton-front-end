366ff6a1b9788ffb54e78c46367bd4feec5ff2c8
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-02-03 14:04:01 -0500

    Create utils, 'apiUtils.js', 'locationUtils.js'.

---
+++ src/utils/apiUtils.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const VITE_BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const getGoogleMapsData = async (requestBody) => {
+	try {
+		const res = await axios.post(`${VITE_BASE_URL}`, requestBody);
+		return res;
+	} catch (err) {
+		console.error(err);
+	}
+};
---
+++ src/utils/locationUtils.js
@@ -0,0 +1,18 @@
+export const getUserLocation = () => {
+	// getting user position can take some time - as a reuslt we are wrapping in a promise
+	return new Promise((resolve, reject) => {
+		if (!navigator.geolocation) {
+			reject('Geolocation is not supported by this browser.');
+		} else {
+			navigator.geolocation.getCurrentPosition(
+				(position) => {
+					const { latitude, longitude } = position.coords;
+					resolve({ latitude, longitude });
+				},
+				(err) => {
+					reject(err.message);
+				}
+			);
+		}
+	});
+};
